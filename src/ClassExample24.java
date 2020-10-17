1bb017c9fd02e7283ac82290bcbeecdad3d7c074
Álvaro Gascón <alvaro.gascon.m@gmail.com>
2020-10-17 19:28:24 +0200

    Initial commit

---
+++ src/ClassExample24.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Example24 {
+    public static void main (String argv[]) {
+        int n, f;
+        System.out.println("Enter a number:");
+//Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        n = inputValue.nextInt();
+        f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        System.out.println(f);
+    }
+}
